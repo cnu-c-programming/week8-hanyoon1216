47e60c9ff5e0f8cb25083554cd063a5e4ba20842
hanyoon1216 <hanyoon1216@gmail.com>
2026-05-01 22:38:43 +0900

    Fix string comparison function and update test cases

--- code/1-4.c
+++ code/1-4.c
@@ -5,15 +5,16 @@ int my_strcmp(const char *a, const char *b) {
     if (*a != *b) { 
       return *a - *b; 
     } 
-    a++; b++; 
+    a++; 
+    b++; 
   }
   
   return 0; 
 } 
 
 int main() { 
+  printf("%d\n", my_strcmp("abc", "abcd")); 
   printf("%d\n", my_strcmp("abc", "abc")); 
-  printf("%d\n", my_strcmp("abc", "abd")); 
   printf("%d\n", my_strcmp("b", "a")); 
   
   return 0; 
