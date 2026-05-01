21cd94f7ec09cc0f483eb25b8424d42fc8dbeb00
hanyoon1216 <hanyoon1216@gmail.com>
2026-05-01 22:28:20 +0900

    Implement string comparison function my_strcmp

---
+++ code/1-4.c
@@ -0,0 +1,20 @@
+#include <stdio.h> 
+
+int my_strcmp(const char *a, const char *b) { 
+  while (*a != '\0' || *b != '\0') { 
+    if (*a != *b) { 
+      return *a - *b; 
+    } 
+    a++; b++; 
+  }
+  
+  return 0; 
+} 
+
+int main() { 
+  printf("%d\n", my_strcmp("abc", "abc")); 
+  printf("%d\n", my_strcmp("abc", "abd")); 
+  printf("%d\n", my_strcmp("b", "a")); 
+  
+  return 0; 
+}
